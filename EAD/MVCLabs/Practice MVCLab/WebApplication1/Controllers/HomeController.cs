d0b02ba84b7199c5acd9df664a83d244f2d5f617
unknown <X00102929@lab227-16.computing.stu.it-tallaght.ie>
2015-11-26 11:55:47 +0000

    Signed-off-by: unknown <X00102929@lab227-16.computing.stu.it-tallaght.ie>

---
+++ EAD/MVCLabs/Practice MVCLab/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Person()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Person(Models.Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("ConfirmPerson", person);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult ConfirmPerson(Models.Person person)
+        {
+
+            return View(person);
+
+        }
+    }
+}
---
+++ EAD/MVCLabs/Practice MVCLab/WebApplication1/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Person
+    {
+        [Required (ErrorMessage = "Requires a name")]
+        [StringLength (100, MinimumLength =10, ErrorMessage ="Must be over 10 characters")]
+        [Display (Name ="Name")]
+        public String name { get; set; }
+
+        [Required(ErrorMessage ="Requires a Phone Number")]
+        [StringLength(10, MinimumLength =10, ErrorMessage ="Must be 10 digits")]
+        [RegularExpression(@"(?:\d*\.)?\d+", ErrorMessage ="Must only be numbers")]
+        [Display(Name = "Phone Number")]
+        public String Phone { get; set; }
+
+        [Required(ErrorMessage ="Requires an Email")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage ="Invalid Email")]
+        [Display(Name = "Email")]
+        public String Email { get; set; }
+
+    }
+}
